a3c73130ef64a6eb1e3544a9b331593635160efa
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2021-02-26 02:58:00 -0800

    developed layout and grid responsiveness

--- employee_directory/src/components/Body.js
+++ employee_directory/src/components/Body.js
@@ -3,18 +3,18 @@ import "../styles/Body.css";
 
 function Body({users}) {
     return (
-        <div className="card">
-            <div className="card-body">
-                <h5 className="card-title">Employees</h5>
+        <div className="d-flex flex-wrap justify-content-between align-item-center">
+            {/* <div className="card-body"> */}
+                {/* <h3 className="card-title">Employees</h3> */}
                 {users.length > 0 ? users.map(user =>  
-                <div className="card">
-                {/* <img src={user.picture.large} className="card-img-top" alt="profile-image"/> */}
+                <div className="card d-flex flex-row">
+                <img src={user.picture.large} alt="profile-image"/>
                     <div className="card-body">
                         <h5 className="card-title">{user.name.first} {user.name.last}</h5>
                     </div>
                 </div>
                 ): ""}
-            </div>
+            {/* </div> */}
         </div>
     )
 }
