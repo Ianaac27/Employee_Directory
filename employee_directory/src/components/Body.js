7425a80ca813b094aa96eb66fc1b7c8317e99124
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2021-02-26 03:30:58 -0800

    updated styling for styling cards and header

--- employee_directory/src/components/Body.js
+++ employee_directory/src/components/Body.js
@@ -4,17 +4,14 @@ import "../styles/Body.css";
 function Body({users}) {
     return (
         <div className="d-flex flex-wrap justify-content-between align-item-center">
-            {/* <div className="card-body"> */}
-                {/* <h3 className="card-title">Employees</h3> */}
                 {users.length > 0 ? users.map(user =>  
                 <div className="card d-flex flex-row">
-                <img src={user.picture.large} alt="profile-image"/>
+                    <img src={user.picture.large} alt="profile-image"/>
                     <div className="card-body">
                         <h5 className="card-title">{user.name.first} {user.name.last}</h5>
                     </div>
                 </div>
                 ): ""}
-            {/* </div> */}
         </div>
     )
 }
