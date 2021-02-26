da06f5b9bd295868bcbe36b5de078c90a05e70e1
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2021-02-25 19:41:48 -0800

    populated app with random person api

--- employee_directory/src/App.js
+++ employee_directory/src/App.js
@@ -1,13 +1,47 @@
+import React from "react";
 import Header from "./components/Header";
+import Filters from "./components/Filters";
 import Body from "./components/Body";
+import API from "./utils/API";
 
-function App() {
-  return (
-    <div className="Container">
-     <Header />
-     <Body />
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    users : [],
+    search: ""
+  }
+
+//look up how to dynamically pass data from API to screen. Anthony says to review previous assignments from Saturday
+
+  componentDidMount() {
+    this.getUsers();
+  }
+
+  getUsers = () => {
+    
+    API.getUsers()
+      .then(res => this.setState({ users: res.data.results }))
+      .catch(err => console.log(err));
+  };
+
+  handleInputChange = event => {
+    const {name, value} = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+ 
+  render() {
+      return (
+        <div className="Container">
+          <Header />
+          <Filters handleInputChange={this.handleInputChange}/>
+          {this.state.search}
+          <Body users={this.state.users} search={this.state.search}/>
+        </div>
+      );
+  }
+
 }
 
 export default App;
--- employee_directory/src/components/Body.js
+++ employee_directory/src/components/Body.js
@@ -1,11 +1,20 @@
-function Body() {
+function Body({users, search}) {
     return (
         <div className="card" style={{width: "18rem;"}}>
             <img src="..." className="card-img-top" alt="..."/>
             <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
+                <h5 className="card-title">Card title</h5>
+                {
+                    users
+                        .filter((user) => {
+                            //check if search is in first or last name
+                            //match with true or false compared to the search
+                            //nuild a concatinate between first and last when checking both names
+                            return true;
+                        })
+                        .map(user =>  <p className="card-text">{user.name.first} {user.name.last}</p>)}
+               
+                <a href="#" className="btn btn-primary">Go somewhere</a>
             </div>
         </div>
     )
